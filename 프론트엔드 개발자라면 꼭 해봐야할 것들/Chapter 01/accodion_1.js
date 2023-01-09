179641b36d3853a9cf0f467af03adbf797d8e333
Sang jin Lee <w1004123@gmail.com>
2023-01-10 00:32:26 +0900

    TEST :: Table animation

--- 프론트엔드 개발자라면 꼭 해봐야할 것들/Chapter 01/accodion_1.js
+++ 프론트엔드 개발자라면 꼭 해봐야할 것들/Chapter 01/accodion_1.js
@@ -5,6 +5,10 @@ window.onload = () => {
     //FAQ 본문 선택
     let $answer1 = document.querySelectorAll('.answer1');
     //console.log($answer1[i]);
+    console.log($answer1);
+
+    const $btn = document.getElementById('btn');
+    console.log($btn);
 
     //반복문을 순회하면서 해당 FAQ 제목 클릭시 콜백 처리
     for(let i=0; i<$container.length; i++) {
@@ -17,5 +21,14 @@ window.onload = () => {
             $answer1[i].classList.add('active');
         });
     }
+
+    $btn.addEventListener('click', () => {
+        //버튼 클릭시 처리할 일
+        console.log('모두 닫기 버튼');
+        
+        for(let i = 0; i< $answer1.length; i++){
+            $answer1[i].classList.remove('active');
+        }
+    });
 }
 
